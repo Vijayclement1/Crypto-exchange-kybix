be6e179755151b4199221f2a411664a944ffac21
vijay clement <vijayclement1@gmail.com>
2024-07-22 12:47:27 +0530

    project added
    initial commit

---
+++ sample.js
@@ -0,0 +1,6 @@
+
+
+function changeparagraph() {
+    var paragraph = document.getElementById("paragraph");
+    paragraph.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur neque minus quis quo quam labore nulla maxime ad enim libero? Laboriosam vitae voluptatum illo adipisci deserunt quia incidunt enim nobis. Possimus in magnam hic itaque minus deleniti voluptatibus, molestias quia adipisci sunt. Perspiciatis eligendi odit possimus dolores quisquam dicta, adipisci vero in, deleniti reprehenderit minima? Amet eos quam obcaecati sequi sed consequuntur nostrum magnam delectus quo nesciunt atque totam corporis, officia error perferendis commodi non provident pariatur autem, praesentium facere ad! Aperiam quis dolorem itaque soluta sed cum nisi, nemo ipsa dolor iure vel voluptate modi cupiditate ex illum expedita quidem veritatis aliquid blanditiis repellat necessitatibus? Unde rem eligendi earum fugiat qui error. quas animi.";
+}
